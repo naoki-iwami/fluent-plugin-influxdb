a737cbf6506a08957da98a19b06e6d8430a711f1
Naoki Iwami <iwami@bizreach.co.jp>
2018-02-23 17:24:00 +0900

    Fluent::ObjectBufferedOutput を継承した形に書き換え。日付の処理方法を変更。

--- lib/fluent/plugin/out_influxdb.rb
+++ lib/fluent/plugin/out_influxdb.rb
@@ -1,83 +1,44 @@
 # encoding: UTF-8
 require 'date'
+require 'excon'
 require 'influxdb'
-require 'fluent/plugin/output'
-require 'fluent/mixin'
+require 'json'
+require 'uri'
+begin
+  require 'strptime'
+rescue LoadError
+end
 
-class Fluent::Plugin::InfluxdbOutput < Fluent::Plugin::Output
-  Fluent::Plugin.register_output('influxdb', self)
+require 'fluent/output'
 
-  helpers :compat_parameters
+class Fluent::InfluxdbObjectOutput < Fluent::ObjectBufferedOutput
+  class ConnectionFailure < StandardError; end
 
-  DEFAULT_BUFFER_TYPE = "memory"
+  Fluent::Plugin.register_output('influxdb_object', self)
 
-  config_param :host, :string,  default: 'localhost',
-               desc: "The IP or domain of influxDB, separate with comma."
-  config_param :port, :integer,  default: 8086,
-               desc: "The HTTP port of influxDB."
-  config_param :dbname, :string,  default: 'fluentd',
-               desc: <<-DESC
-The database name of influxDB.
-You should create the database and grant permissions at first.
-DESC
-  config_param :measurement, :string, default: nil,
-               desc: "The measurement name to insert events. If not specified, fluentd's tag is used"
-  config_param :user, :string,  default: 'root',
-               desc: "The DB user of influxDB, should be created manually."
-  config_param :password, :string,  default: 'root', secret: true,
-               desc: "The password of the user."
-  config_param :retry, :integer, default: nil,
-               desc: 'The finite number of retry times. default is infinite'
-  config_param :time_key, :string, default: 'time',
-               desc: 'Use value of this tag if it exists in event instead of event timestamp'
-  config_param :time_precision, :string, default: 's',
-               desc: <<-DESC
-The time precision of timestamp.
-You should specify either hour (h), minutes (m), second (s),
-millisecond (ms), microsecond (u), or nanosecond (ns).
-DESC
-  config_param :use_ssl, :bool, default: false,
-               desc: "Use SSL when connecting to influxDB."
-  config_param :verify_ssl, :bool, default: true,
-               desc: "Enable/Disable SSL Certs verification when connecting to influxDB via SSL."
-  config_param :auto_tags, :bool, default: false,
-               desc: "Enable/Disable auto-tagging behaviour which makes strings tags."
-  config_param :tag_keys, :array, default: [],
-               desc: "The names of the keys to use as influxDB tags."
-  config_param :sequence_tag, :string, default: nil,
-               desc: <<-DESC
-The name of the tag whose value is incremented for the consecutive simultaneous
-events and reset to zero for a new event with the different timestamp.
-DESC
-  config_param :retention_policy_key, :string, default: nil,
-               desc: "The key of the key in the record that stores the retention policy name"
-  config_param :default_retention_policy, :string, default: nil,
-               desc: "The name of the default retention policy"
-
-  config_section :buffer do
-    config_set_default :@type, DEFAULT_BUFFER_TYPE
-    config_set_default :chunk_keys, ['tag']
-  end
+  config_param :host, :string,  default: 'localhost'
+  config_param :port, :integer,  default: 8086
+  config_param :dbname, :string,  default: 'fluentd'
+  config_param :measurement, :string, default: nil
+  config_param :user, :string,  default: 'root'
+  config_param :password, :string,  default: 'root', secret: true
+  config_param :retry, :integer, default: nil
+  config_param :time_key, :string, default: 'time'
+  config_param :time_precision, :string, default: 's'
+  config_param :use_ssl, :bool, default: false
+  config_param :verify_ssl, :bool, default: true
+  config_param :tag_keys, :array, default: []
 
   def initialize
     super
-    @seq = 0
-    @prev_timestamp = nil
   end
 
   def configure(conf)
-    compat_parameters_convert(conf, :buffer)
     super
-    @time_precise = time_precise_lambda()
-    raise Fluent::ConfigError, "'tag' in chunk_keys is required." if not @chunk_key_tag
-  end
-
-  def start
-    super
+    log.info "configure"
+    @time_parser = create_time_parser
 
     log.info "Connecting to database: #{@dbname}, host: #{@host}, port: #{@port}, username: #{@user}, use_ssl = #{@use_ssl}, verify_ssl = #{@verify_ssl}"
-
-    # ||= for testing.
     @influxdb ||= InfluxDB::Client.new @dbname, hosts: @host.split(','),
                                                 port: @port,
                                                 username: @user,
@@ -93,70 +54,73 @@ DESC
       unless existing_databases.include? @dbname
         raise Fluent::ConfigError, 'Database ' + @dbname + ' doesn\'t exist. Create it first, please. Existing databases: ' + existing_databases.join(',')
       end
+      log.info 'OK: Database ' + @dbname + ' exist.'
     rescue InfluxDB::AuthenticationError, InfluxDB::Error
       log.info "skip database presence check because '#{@user}' user doesn't have admin privilege. Check '#{@dbname}' exists on influxdb"
     end
+
   end
 
-  FORMATTED_RESULT_FOR_INVALID_RECORD = ''.freeze
-
-  def format(tag, time, record)
-    # TODO: Use tag based chunk separation for more reliability
-    if record.empty? || record.has_value?(nil)
-      FORMATTED_RESULT_FOR_INVALID_RECORD
+  # once fluent v0.14 is released we might be able to use
+  # Fluent::Parser::TimeParser, but it doesn't quite do what we want - if gives
+  # [sec,nsec] where as we want something we can call `strftime` on...
+  def create_time_parser
+    if @time_key_format
+      begin
+        # Strptime doesn't support all formats, but for those it does it's
+        # blazingly fast.
+        strptime = Strptime.new(@time_key_format)
+        Proc.new { |value| strptime.exec(value).to_datetime }
+      rescue
+        # Can happen if Strptime doesn't recognize the format; or
+        # if strptime couldn't be required (because it's not installed -- it's
+        # ruby 2 only)
+        Proc.new { |value| DateTime.strptime(value, @time_key_format) }
+      end
     else
-      [precision_time(time), record].to_msgpack
+      Proc.new { |value| DateTime.parse(value) }
     end
   end
 
-  def shutdown
-    super
-    @influxdb.stop!
+  def parse_time(value, event_time, tag)
+    @time_parser.call(value)
+  rescue => e
+    router.emit_error_event(@time_parse_error_tag, Fluent::Engine.now, {'tag' => tag, 'time' => event_time, 'format' => @time_key_format, 'value' => value}, e)
+    return Time.at(event_time).to_datetime
   end
 
-  def formatted_to_msgpack_binary
-    true
-  end
 
-  def multi_workers_ready?
-    true
-  end
-
-  def write(chunk)
-    points = []
+  def write_objects(tag, chunk)
     tag = chunk.metadata.tag
+    points = []
+
     chunk.msgpack_each do |time, record|
-      timestamp = record.delete(@time_key) || time
-      if tag_keys.empty? && !@auto_tags
-        values = record
-        tags = {}
-      else
-        values = {}
-        tags = {}
-        record.each_pair do |k, v|
-          if (@auto_tags && v.is_a?(String)) || @tag_keys.include?(k)
-            # If the tag value is not nil, empty, or a space, add the tag
-            if v.to_s.strip != ''
-              tags[k] = v
-            end
-          else
-            values[k] = v
-          end
-        end
-      end
-      if @sequence_tag
-        if @prev_timestamp == timestamp
-          @seq += 1
+      next unless record.is_a? Hash
+#      timestamp = record.delete(@time_key) || time
+#      timestamp = DateTime.strptime(record.delete(@time_key), @time_key_format).to_time.to_i || time
+      timestamp =
+        if @time_key_format
+          DateTime.strptime(record.delete(@time_key), @time_key_format).to_time.to_i || time
         else
-          @seq = 0
+          DateTime.parse(record.delete(@time_key)).to_time.to_i || time
         end
-        tags[@sequence_tag] = @seq
-        @prev_timestamp = timestamp
+      log.info timestamp
+
+      values = {}
+      tags = {}
+
+      record.each_pair do |k, v|
+        if v.is_a? Numeric then
+          values[k] = v
+        end
+        if @tag_keys.include?(k) then
+          tags[k] = v if v.to_s.strip != ''
+        end
       end
 
       if values.empty?
-          log.warn "Skip record '#{record}', because InfluxDB requires at least one value in raw"
-          next
+        log.warn "Skip record '#{record}', because InfluxDB requires at least one value in raw"
+        next
       end
 
       point = {
@@ -165,56 +129,16 @@ DESC
         values: values,
         tags: tags,
       }
-      retention_policy = @default_retention_policy
-      unless @retention_policy_key.nil?
-        retention_policy = record.delete(@retention_policy_key) || @default_retention_policy
-        unless points.nil?
-          if retention_policy != @default_retention_policy
-            # flush the retention policy first
-            @influxdb.write_points(points, nil, @default_retention_policy)
-            points = nil
-          end
-        end
-      end
-      if points.nil?
-        @influxdb.write_points([point], nil, retention_policy)
-      else
-        points << point
-      end
+      # log.info timestamp
+
+      points << point
     end
 
-    unless points.nil?
-      if @default_retention_policy.nil?
-        @influxdb.write_points(points)
-      else
-        @influxdb.write_points(points, nil, @default_retention_policy)
-      end
+    if points.size > 0 then
+      log.info "write points size: #{points.size}"
+      @influxdb.write_points(points)
     end
+
   end
 
-  def time_precise_lambda()
-    case @time_precision.to_sym
-    when :h then
-      lambda{|nstime| nstime / (10 ** 9) / (60 ** 2) }
-    when :m then
-      lambda{|nstime| nstime / (10 ** 9) / 60 }
-    when :s then
-      lambda{|nstime| nstime / (10 ** 9) }
-    when :ms then
-      lambda{|nstime| nstime / (10 ** 6) }
-    when :u then
-      lambda{|nstime| nstime / (10 ** 3) }
-    when :ns then
-      lambda{|nstime| nstime }
-    else
-      raise Fluent::ConfigError, 'time_precision ' + @time_precision + ' is invalid.' +
-        'should specify either either hour (h), minutes (m), second (s), millisecond (ms), microsecond (u), or nanosecond (ns)'
-    end
-  end
-
-  def precision_time(time)
-    # nsec is supported from v0.14
-    nstime = time * (10 ** 9) + (time.is_a?(Integer) ? 0 : time.nsec)
-    @time_precise.call(nstime)
-  end
 end
